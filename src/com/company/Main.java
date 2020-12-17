7977d59e38b258277ec0513ef037d6855a378862
e999or <e999or@bk.ru>
2020-12-17 03:49:23 +0300

    add conditition and comments
    add condition for files with different count strings
    all lines add comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,26 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-    BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("C:\\Users\\Eg\\Desktop\\1.log")));
-    FileWriter writer = new FileWriter("C:\\Users\\Eg\\Desktop\\2.txt",true);
-    String line = bufferedReader.readLine();
-    int i = 0;
-        while (line != null){
-            if(line.contains("DataForMerchant:>") || i > 0){
-                i++;
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(new File("C:\\Users\\Eg\\Desktop\\1.log")));//открываем поток получаем файл
+        FileWriter writer = new FileWriter("C:\\Users\\Eg\\Desktop\\2.txt", true);//открываем 2ой поток для записи в файл , каждая запись в файл будет проиводится с сохранением последней
+        String line = bufferedReader.readLine();//получаем целую строку
+        int i = 0;//счётчик строк
+        while (line != null) {//начинаем цикл пока строки не будут пустыми
+            if (line.contains("DataForMerchant:>") || i > 0) {//ищем оригинальное слово
+                i++;//прибавляем к счётчику
             }
 
-            if(i == 1){
-                line = bufferedReader.readLine();
-                i = 0;
-                SplitString splStr = new SplitString(line);
-                writer.write( splStr.check + System.lineSeparator());
-                writer.flush();
+            if (i >= 1 && line.contains("Ret:>")) {//получаем положительный ответ ,что есть фин запрос и что строка содержит Ret(чек)
+                i = 0;//обнуляем счётчик
+                SplitString splStr = new SplitString(line);// создаём обеъкт класса и передаём ему строку
+                writer.write(splStr.check + System.lineSeparator());//разбиваем строку, и записываем чать массива
+                writer.flush();//записываем в поток все данные
             }
-            line = bufferedReader.readLine();
+            line = bufferedReader.readLine();//переходим на другую строку
 
         }
-        bufferedReader.close();
-        writer.close();
+        bufferedReader.close();//закрываем поток
+        writer.close();//закрываем поток
 
     }
 
--- src/com/company/SplitString.java
+++ src/com/company/SplitString.java
@@ -2,10 +2,10 @@ package com.company;
 
 public class SplitString {
     String check;
+
     public SplitString(String s) {
-        String[] words = s.split(".Ret:>");
-        String[] finalCheck = words[1].split("<:");
-        check = finalCheck[0];
-        System.out.println(check);
+        String[] words = s.split(".Ret:>");//разбиваем строку до Ret:>
+        String[] finalCheck = words[1].split("<:");//разбиваем строку после: <:
+        check = finalCheck[0];//получаем чек без хвостов
     }
 }
